6f133aabf29e98c163ce9aad36a4fe64d26757eb
Versina Vasile <ipvvg4@gmail.com>
2023-11-28 20:24:38 +0100

    add homework 34

---
+++ src/homework34/task02/Main.java
@@ -0,0 +1,81 @@
+package homework34.task02;
+
+import java.util.Scanner;
+
+public class Main {
+  /*
+  В классе Main сделать метод, который выведет тип планеты:
+Безжизненный камень, обитаемаая планета, газовый гигант или ледяная планета.
+В методе main предложить пользователю выбрать планету и в ответ вывести класификацию этой планеты,
+используя разработанный ранее метод.
+   */
+
+  public static String getPlanetName(int choice) {
+    switch (choice) {
+      case 1:
+        return "Меркур";
+      case 2:
+        return "Венера";
+      case 3:
+        return "Земля";
+      case 4:
+        return "Марс";
+      case 5:
+        return "Юпитер";
+      case 6:
+        return "Сатурн";
+      case 7:
+        return "Уран";
+      case 8:
+        return "Нептун";
+      default:
+        return "Неизвестная планета";
+    }
+  }
+
+  public static String getPlanetType(String planetName) {
+    String planetType;
+    switch (planetName) {
+      case "Меркур":
+      case "Венера":
+      case "Земля":
+      case "Марс":
+        planetType = "Обитаемая планета";
+        break;
+      case "Юпитер":
+      case "Сатурн":
+        planetType = "Газовый гигант";
+        break;
+      case "Уран":
+      case "Нептун":
+        planetType = "Ледяная планета";
+        break;
+      default:
+        planetType = "Неизвестная планета";
+        break;
+    }
+    return planetType;
+  }
+
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Выберите планету:");
+    System.out.println("1. Меркур");
+    System.out.println("2. Венера");
+    System.out.println("3. Земля");
+    System.out.println("4. Марс");
+    System.out.println("5. Юпитер");
+    System.out.println("6. Сатурн");
+    System.out.println("7. Уран");
+    System.out.println("8. Нептун");
+
+    int choice = scanner.nextInt();
+    String planetName = getPlanetName(choice);
+    System.out.println("Вы выбрали планету: " + planetName);
+    String planetType = getPlanetType(planetName);
+
+    System.out.println("Планета " + planetName + " относится к Класс: " + planetType);
+  }
+}
